45b9195a6d195d25b1e6112443db428f6d635d0d
cmal <zyzy5730@163.com>
2013-11-13 11:13:34 +0800

    modified:   app/controllers/weixins_controller.rb 	modified:   app/helpers/weixins_helper.rb 	modified:   app/views/weixins/echo.xml.erb

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -1,8 +1,9 @@
 class WeixinsController < ApplicationController
 	skip_before_filter :verify_authenticity_token
-	before_filter :check_weixin_legality
+	#before_filter :check_weixin_legality
 	def show
-		render :text=> params[:echostr]
+		#render :text=> params[:echostr]
+    render "echo", :format=>:xml
 	end
 	
 	def create
@@ -13,7 +14,7 @@ class WeixinsController < ApplicationController
 	end
 
 	def check_weixin_legality
-	        #logger.debug params[:timestamp]
+	  #logger.debug params[:timestamp]
 		array =["9c49b86f2d18b762983d9d76b257a257",params[:timestamp],params[:nonce]].sort
 		render :text => "Forbidden",:status => 403 if params[:signature]!=Digest::SHA1.hexdigest(array.join)
 	end
--- app/helpers/weixins_helper.rb
+++ app/helpers/weixins_helper.rb
@@ -1,5 +1,5 @@
 module WeixinsHelper
-  def say
-    "大山的回声: " + params[:xml][:Content]
+  def say(content)
+    "大山的回声: " + content
   end
 end
