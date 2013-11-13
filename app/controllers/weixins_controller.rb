62031c29c4f078a1acf5c3c63b5c168ae7bdadb3
cmal <zyzy5730@163.com>
2013-11-13 16:33:30 +0800

    modified:   app/controllers/weixins_controller.rb 	modified:   app/helpers/weixins_helper.rb 	modified:   app/views/weixins/echo.xml.erb

--- app/controllers/weixins_controller.rb
+++ app/controllers/weixins_controller.rb
@@ -7,8 +7,9 @@ class WeixinsController < ApplicationController
 	end
 	
 	def create
-		logger.debug "text:#{params[:xml]}"
+		#logger.debug "text:#{params[:xml]}"
 		if params[:xml][:MsgType]=="text"
+      @return_content = Object.const_get("Weixins#{params[:xml][:MsgType].capitalize}Handler").new(params[:xml][:Content]).return
 			render "echo",:format=>:xml
 		end
 	end
@@ -18,4 +19,28 @@ class WeixinsController < ApplicationController
 		array =["9c49b86f2d18b762983d9d76b257a257",params[:timestamp],params[:nonce]].sort
 		render :text => "Forbidden",:status => 403 if params[:signature]!=Digest::SHA1.hexdigest(array.join)
 	end
+end
+
+class WeixinsHandler < Object
+  def new(content)
+    content
+  end
+end
+
+class WeixinsTextHandler < WeixinsHandler
+end
+
+class WeixinsImageHandler < WeixinsHandler
+end
+
+class WeixinsVoiceHandler < WeixinsHandler
+end
+
+class WeixinsVideoHandler < WeixinsHandler
+end
+
+class WeixinsMusicHandler < WeixinsHandler
+end
+
+class WeixinsNewsHandler < WeixinsHandler
 end
--- app/helpers/weixins_helper.rb
+++ app/helpers/weixins_helper.rb
@@ -1,6 +1,2 @@
 module WeixinsHelper
-  def say
-    params[:xml][:Content]
-    #"计财部的回声: " + content
-  end
 end
