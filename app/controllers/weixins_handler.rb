60ebe94ac10bb1ac4999c975eb6480abfcc04f91
cmal <zyzy5730@163.com>
2013-11-13 16:34:53 +0800

    new file:   app/controllers/weixins_handler.rb

---
+++ app/controllers/weixins_handler.rb
@@ -0,0 +1,23 @@
+class WeixinsHandler < Object
+  def new(content)
+    content
+  end
+end
+
+class WeixinsTextHandler < WeixinsHandler
+end
+
+class WeixinsImageHandler < WeixinsHandler
+end
+
+class WeixinsVoiceHandler < WeixinsHandler
+end
+
+class WeixinsVideoHandler < WeixinsHandler
+end
+
+class WeixinsMusicHandler < WeixinsHandler
+end
+
+class WeixinsNewsHandler < WeixinsHandler
+end
