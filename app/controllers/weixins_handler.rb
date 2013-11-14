438b12bae5ac0b3d6c2948b74c1b68f32c09039c
cmal <zyzy5730@163.com>
2013-11-14 17:44:08 +0800

    modified:   app/controllers/weixins_handler.rb

--- app/controllers/weixins_handler.rb
+++ app/controllers/weixins_handler.rb
@@ -9,6 +9,22 @@ class WeixinsHandler < Object
 end
 
 class WeixinsTextHandler < WeixinsHandler
+  commands = {"shibor" => {:method => :query_shibor},
+              "Shibor" => {:method => :query_shibor}
+             }
+  def return
+    if valid_command?
+      send commands[command][:method]
+    else
+      super
+    end
+  end
+  def valid_command?
+    commands.has_key? command
+  end
+  def query_shibor
+    @content = "http://www.shibor.org/shibor/web/html/shibor.html"
+  end
 end
 
 class WeixinsImageHandler < WeixinsHandler
