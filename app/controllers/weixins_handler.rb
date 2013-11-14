646c3a9ef12a28935e7369e3cfc50f1aa4e8531b
cmal <zyzy5730@163.com>
2013-11-14 18:19:09 +0800

    modified:   weixins_handler.rb 	new file:   weixins_text_handler.rb

--- app/controllers/weixins_handler.rb
+++ app/controllers/weixins_handler.rb
@@ -7,27 +7,6 @@ class WeixinsHandler < Object
     @result
   end
 end
-
-class WeixinsTextHandler < WeixinsHandler
-  Commands = {"shibor" => {:method => :query_shibor},
-              "Shibor" => {:method => :query_shibor}
-             }
-  def return
-    if valid_command?
-      send Commands[@content][:method]
-    else
-      @result = "请选择关键词：shibor,"
-    end
-    super
-  end
-  def valid_command?
-    Commands.has_key? @content
-  end
-  def query_shibor
-    @result = "http://www.shibor.org/shibor/web/html/shibor.html"
-  end
-end
-
 class WeixinsImageHandler < WeixinsHandler
 end
 
